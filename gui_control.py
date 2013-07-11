171049f89f7e30b16d1dce629e17c1aa4787ffaf
Alex Young <alexyoung91@googlemail.com>
2013-07-11 13:28:44 +0100

    Added window framework

--- gui_control.py
+++ gui_control.py
@@ -1,3 +1,12 @@
+from RobotArm import *
 from Tkinter import *
 
+#arm = RobotArm()
+#arm.setMode(PROGRAMMABLE)
+
 root = Tk()
+root.title("Robot Arm Controller")
+root.geometry("300x200")
+root.resizable(width = False, height = False)
+
+root.mainloop()
